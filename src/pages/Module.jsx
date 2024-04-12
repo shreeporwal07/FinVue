e1c57f4f88899deecd384c887276c817c619d781
shreeporwal07 <shreeporwal07@gmail.com>
2024-04-13 00:11:46 +0530

    modules css updated

--- src/pages/Module.jsx
+++ src/pages/Module.jsx
@@ -8,12 +8,12 @@ function Module() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
     return (
-        <div className="flex h-screen overflow-hidden bg-[#181A2E]">
+        <div className="flex overflow-hidden bg-[#181A2E]">
             <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            <div className="relative flex flex-col overflow-y-auto overflow-x-hidden">
                 <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-                <div className="px-20 py-10">
-                    <span className="font-kalam text-2xl md:text-5xl text-slate-100 font-bold mb-8">Modules</span>
+                <div className="m-0.5 md:m-8 space-y-8">
+                    <span className="font-kalam sm: text-2xl md:text-5xl text-slate-100 font-bold">Modules</span>
                     <Module1/>
                 </div>
                

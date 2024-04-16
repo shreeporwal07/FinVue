6e1594921120645c6d6363c00f35701b9b411db2
shreeporwal07 <shreeporwal07@gmail.com>
2024-04-16 17:17:00 +0530

    progress css

--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -52,16 +52,17 @@ const Learning = () => {
                 <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
                 <div className="m-10">
                     <span className="font-kalam text-2xl md:text-5xl text-slate-100 font-bold mb-8">Start Learnings</span>
-                    <div className="relative bg-gray-200 w-full h-6 rounded">
-                        <div className="flex justify-center absolute top-0 left-0 h-full bg-green-500 rounded" style={{ width: `${Math.min(totalscore, 50)}%`, transition: 'width 1s ease-in-out' }}></div>
+                    <h3 className="font-inter mt-8 font-semibold">Your Progress</h3>
+                    <div className="relative bg-gray-200 w-full h-3 rounded-3xl mb-8">
+                        <div className="flex justify-center absolute top-0 left-0 h-full bg-green-500 rounded-3xl" style={{ width: `${Math.min(totalscore, 50)}%`, transition: 'width 1s ease-in-out' }}></div>
                     </div>
 
-                    <div className="flex flex-col mt-4 gap-10">
+                    <div className="flex flex-col mt-4 gap-6">
                         {modules.map(module => (
                             <div key={module.id} className="flex py-5 px-5  flex-col gap-10 col-span-full xl:col-span-6 bg-gradient-to-r from-[#434974] to-[#242949] shadow-lg rounded-lg border border-slate-700">
                                 <Link to={isAuthenticated ? module.path : ''} onClick={isAuthenticated ? undefined : loginWithRedirect}>
                                     <div className="flex justify-between ">
-                                        <span className="text-2xl cursor-pointer">{module.title}</span>
+                                        <span className="text-2xl cursor-pointer font-inter">{module.title}</span>
                                         {user ? (<span className="text-xl">{score && score[module.id - 1] ? <>{(score[module.id - 1])}/10</> : <></>}</span>) : <></>}
                                     </div>
 
